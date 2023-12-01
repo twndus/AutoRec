83c26e7441b04c9099515aa1f49cfe297332928a
twndus <twndus1362@gmail.com>
2023-12-01 11:23:13 +0900

    feat: define model

--- model/autorec.py
+++ model/autorec.py
@@ -6,11 +6,22 @@ from torch import nn
 
 class AutoRec(nn.Module):
 
-    def __init__(self, input_dim, latent_dim, output_func='sigmoid'):
+    def __init__(self, input_dim, latent_dim):
         
         super(AutoRec, self).__init__()
-        pass
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+
+        # parameters
+        self.encoder = nn.Embedding(self.input_dim, self_latent_dim)
+        self.decoder = nn.Embedding(self.latent_dim, self.input_dim)
+        
+        # function
+        self.sigmoid = nn.Sigmoid()
 
     
     def forward(self, x):
-        return x
+        x = self.encoder(x)
+        x = self.sigmoid(x)
+        x = self.decoder(x)
+        return self.sigmoid(x)
